493f8b434046d205a23592692ede1a1d81ea0307
Laura Quintero <llquintero@MacBook-Pro-de-Laura-2.local>
2019-06-17 21:59:21 -0500

    added plural name, verbose and str def to models

--- turnero/turnero_app/models.py
+++ turnero/turnero_app/models.py
@@ -4,9 +4,23 @@ class Prioridad(models.Model):
     nombre = models.CharField(max_length=200)
     numero = models.IntegerField()
 
+    class Meta:
+        verbose_name_plural = "Prioridades"
+        verbose_name = "Prioridad"
+
+    def __str__(self):
+        return "{} {}".format(self.nombre, self.numbero)
+
 class Servicios(models.Model):
     nombre = models.CharField(max_length=200)
 
+    class Meta:
+        verbose_name_plural = "Servicio"
+        verbose_name = "Servicios"
+
+    def __str__(self):
+        return "{}".format(self.nombre)
+        
 class ServiciosUsuarios(models.Model):
     servicicios = models.ForeignKey(Servicios, on_delete=models.CASCADE)
     prioridad = models.ForeignKey(Prioridad, on_delete=models.CASCADE)
@@ -16,4 +30,4 @@ class ServiciosUsuarios(models.Model):
         verbose_name = "Servicios de usuarios"
 
     def __str__(self):
-        return "{} {} {}".format(self.prioridad.nombre,self.servicicios.nombre,self.pk)
+        return "{} {} {}".format(self.pk,self.servicicios,self.prioridad)
